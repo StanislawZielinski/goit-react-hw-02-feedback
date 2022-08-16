024b7b5fd9be2d900651e99a756bd0803aa71349
StanislawZielinski <stanislaw.zielinski.poczta@gmail.com>
2022-08-16 14:50:42 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,31 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  handleChange = (evt) => {
+    evt.preventDefault();
+    console.log("aaa");
+  };
+
+  render() {
+    return (
+      <div>
+        PLEASE LEAVE FEEDBACK
+        <form>
+          <button className="button" type="click" onClick={this.handleChange}>Good</button>
+          <button className="button" type="click" onClick={this.handleChange}>Bad</button>
+          <button className="button" type="click" onClick={this.handleChange}>Neutral</button>
+        </form>
+        </div>
+    );
+  }
+}
+
 export const App = () => {
   return (
     <div
@@ -10,7 +38,9 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      
+      <Counter />
+
     </div>
   );
 };
